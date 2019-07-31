9db6b546088a962d5985356810cdfcaee0363a5d
lbrande <lovbra00@gmail.com>
2019-07-31 22:23:26 +0200

    Implemented rest of logic for parsing a TileSet. Changed from_string functions to use FromStr. Changed get_name to use Display.

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,7 +1,7 @@
-#![allow(dead_code)]
-
 use std::collections::HashSet;
+use std::fmt::{Display, Formatter, Result as FmtResult};
 use std::hash::*;
+use std::str::FromStr;
 use std::u32;
 use toml::Value;
 
@@ -18,27 +18,43 @@ static COLOR_MISSING: &str = "color is missing";
 static COLOR_TYPEERROR: &str = "color is not of type String";
 static UPGRADES_MISSING: &str = "upgrades is missing";
 static UPGRADES_TYPEERROR: &str = "upgrades is not of type Array";
-static UPGRADE_TYPEERROR: &str = "upgrades is not of type Integer";
+static UPGRADE_TYPEERROR: &str = "upgrade is not of type Integer";
+static EDGES_MISSING: &str = "edges is missing";
+static EDGES_TYPEERROR: &str = "edges is not of type Array";
+static EDGE_TYPEERROR: &str = "edge is not of type Integer";
+static VALUE_MISSING: &str = "value is missing";
+static VALUE_TYPEERROR: &str = "value is not of type Integer";
+static STATIONS_TYPEERROR: &str = "stations is not of type Array";
+static STATION_TYPEERROR: &str = "station is not of type String";
+static SPOTS_MISSING: &str = "spots is missing";
+static SPOTS_TYPEERROR: &str = "spots is not of type Integer";
+static VALUES_MISSING: &str = "values is missing";
+static VALUES_TYPEERROR: &str = "values is not of type Array";
+static VALUES_LENERROR: &str = "values is not of length four";
 
+#[derive(Debug)]
 pub struct TileSet {
     tiles: HashSet<(Tile, i32)>,
 }
 
-impl TileSet {
-    pub fn from_string(string: &str) -> Self {
-        let toml = string.parse::<Value>().expect(INVALID_TOML);
+impl FromStr for TileSet {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let toml = s.parse::<Value>().or(Err(INVALID_TOML))?;
         let mut tiles = HashSet::new();
-        let tiles_toml = toml.get("tiles").expect(TILES_MISSING);
-        for value in tiles_toml.as_array().expect(TILES_TYPEERROR) {
+        let tiles_toml = toml.get("tiles").ok_or(TILES_MISSING)?;
+        for value in tiles_toml.as_array().ok_or(TILES_TYPEERROR)? {
             let tile = Tile::from_toml(value);
-            let count = value.get("count").expect(COUNT_MISSING);
-            let count = count.as_integer().expect(COUNT_TYPEERROR);
+            let count = value.get("count").ok_or(COUNT_MISSING)?;
+            let count = count.as_integer().ok_or(COUNT_TYPEERROR)?;
             tiles.insert((tile, count as i32));
         }
-        Self { tiles }
+        Ok(Self { tiles })
     }
 }
 
+#[derive(Debug)]
 pub struct Tile {
     id: i32,
     rails: Vec<Rail>,
@@ -65,7 +81,7 @@ impl Tile {
         Self {
             id,
             rails,
-            color: ColorId::from_string(color),
+            color: color.parse::<ColorId>().unwrap(),
             upgrades,
         }
     }
@@ -85,6 +101,7 @@ impl Hash for Tile {
     }
 }
 
+#[derive(Debug)]
 pub struct Rail {
     edges: Vec<u32>,
     stop: Option<Stop>,
@@ -92,18 +109,36 @@ pub struct Rail {
 
 impl Rail {
     pub fn from_toml(toml: &Value) -> Self {
-        Self {
-            edges: Vec::new(),
-            stop: None,
+        let mut edges = Vec::new();
+        let edges_toml = toml.get("edges").expect(EDGES_MISSING);
+        for value in edges_toml.as_array().expect(EDGES_TYPEERROR) {
+            edges.push(value.as_integer().expect(EDGE_TYPEERROR) as u32);
+        }
+        if let Some(value) = toml.get("city") {
+            let city = City::from_toml(value);
+            Self {
+                edges,
+                stop: Some(Stop::City(city)),
+            }
+        } else if let Some(value) = toml.get("location") {
+            let location = Location::from_toml(value);
+            Self {
+                edges,
+                stop: Some(Stop::Location(location)),
+            }
+        } else {
+            Self { edges, stop: None }
         }
     }
 }
 
+#[derive(Debug)]
 pub enum Stop {
     City(City),
     Location(Location),
 }
 
+#[derive(Debug)]
 pub struct City {
     value: u32,
     stations: HashSet<PubComId>,
@@ -111,44 +146,57 @@ pub struct City {
 }
 
 impl City {
-    pub fn new(value: u32, spots: u32) -> Self {
-        Self {
-            value,
-            stations: HashSet::new(),
-            spots,
+    pub fn from_toml(toml: &Value) -> Self {
+        let value = toml.get("value").expect(VALUE_MISSING);
+        let value = value.as_integer().expect(VALUE_TYPEERROR) as u32;
+        let mut stations = HashSet::new();
+        if let Some(stations_toml) = toml.get("stations") {
+            for value in stations_toml.as_array().expect(STATIONS_TYPEERROR) {
+                let station = value.as_str().expect(STATION_TYPEERROR);
+                stations.insert(station.parse::<PubComId>().unwrap());
+            }
         }
-    }
-
-    pub fn with_station(value: u32, spots: u32, station: PubComId) -> Self {
+        let spots = toml.get("spots").expect(SPOTS_MISSING);
+        let spots = spots.as_integer().expect(SPOTS_TYPEERROR) as u32;
         Self {
             value,
-            stations: vec![station].iter().copied().collect(),
+            stations,
             spots,
         }
     }
 }
 
+#[derive(Debug)]
 pub struct Location {
     values: (u32, u32, u32, u32),
     stations: HashSet<PubComId>,
 }
 
 impl Location {
-    pub fn new(values: (u32, u32, u32, u32)) -> Self {
-        Self {
-            values,
-            stations: HashSet::new(),
+    pub fn from_toml(toml: &Value) -> Self {
+        let values = toml.get("values").expect(VALUES_MISSING);
+        let values = values.as_array().expect(VALUES_TYPEERROR);
+        if values.len() != 4 {
+            panic!(VALUES_LENERROR);
         }
-    }
-
-    pub fn with_station(values: (u32, u32, u32, u32), station: PubComId) -> Self {
-        Self {
-            values,
-            stations: vec![station].iter().copied().collect(),
+        let values = (
+            values[0].as_integer().expect(VALUE_TYPEERROR) as u32,
+            values[1].as_integer().expect(VALUE_TYPEERROR) as u32,
+            values[2].as_integer().expect(VALUE_TYPEERROR) as u32,
+            values[3].as_integer().expect(VALUE_TYPEERROR) as u32,
+        );
+        let mut stations = HashSet::new();
+        if let Some(stations_toml) = toml.get("stations") {
+            for value in stations_toml.as_array().expect(STATIONS_TYPEERROR) {
+                let station = value.as_str().expect(STATION_TYPEERROR);
+                stations.insert(station.parse::<PubComId>().unwrap());
+            }
         }
+        Self { values, stations }
     }
 }
 
+#[derive(Debug)]
 pub enum ColorId {
     Yellow,
     Green,
@@ -157,20 +205,22 @@ pub enum ColorId {
     Red,
 }
 
-impl ColorId {
-    pub fn from_string(string: &str) -> Self {
-        match string {
-            "Yellow" => ColorId::Yellow,
-            "Green" => ColorId::Green,
-            "Brown" => ColorId::Brown,
-            "Gray" => ColorId::Gray,
-            "Red" => ColorId::Red,
-            _ => panic!("{} can not be parsed as ColorId", string),
+impl FromStr for ColorId {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Yellow" => Ok(ColorId::Yellow),
+            "Green" => Ok(ColorId::Green),
+            "Brown" => Ok(ColorId::Brown),
+            "Gray" => Ok(ColorId::Gray),
+            "Red" => Ok(ColorId::Red),
+            _ => Err(format!("{} can not be parsed as ColorId", s)),
         }
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub enum PubComId {
     BAndO,
     CAndA,
@@ -185,19 +235,6 @@ pub enum PubComId {
 impl PubComId {
     pub fn get_name(&self) -> &str {
         match self {
-            PubComId::BAndO => "B&O",
-            PubComId::CAndA => "C&A",
-            PubComId::CAndO => "C&O",
-            PubComId::LV => "LV",
-            PubComId::NAndW => "N&W",
-            PubComId::PRR => "PRR",
-            PubComId::PAndLE => "P&LE",
-            PubComId::SRR => "SRR",
-        }
-    }
-
-    pub fn get_full_name(&self) -> &str {
-        match self {
             PubComId::BAndO => "Baltimore and Ohio Railroad",
             PubComId::CAndA => "Camden and Amboy Railroad",
             PubComId::CAndO => "Chesapeake and Ohio Railroad",
@@ -206,6 +243,39 @@ impl PubComId {
             PubComId::PRR => "Pennsylvania Railroad",
             PubComId::PAndLE => "Pittsburgh & Lake Erie Railroad",
             PubComId::SRR => "Strasburg Rail Road",
+        }
+    }
+}
+
+impl Display for PubComId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            PubComId::BAndO => write!(f, "B&O"),
+            PubComId::CAndA => write!(f, "C&A"),
+            PubComId::CAndO => write!(f, "C&O"),
+            PubComId::LV => write!(f, "LV"),
+            PubComId::NAndW => write!(f, "N&W"),
+            PubComId::PRR => write!(f, "PRR"),
+            PubComId::PAndLE => write!(f, "P&LE"),
+            PubComId::SRR => write!(f, "SRR"),
+        }
+    }
+}
+
+impl FromStr for PubComId {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "B&O" => Ok(PubComId::BAndO),
+            "C&A" => Ok(PubComId::CAndA),
+            "C&O" => Ok(PubComId::CAndO),
+            "LV" => Ok(PubComId::LV),
+            "N&W" => Ok(PubComId::NAndW),
+            "PRR" => Ok(PubComId::PRR),
+            "P&LE" => Ok(PubComId::PAndLE),
+            "SRR" => Ok(PubComId::SRR),
+            _ => Err(format!("{} can not be parsed as PubComId", s)),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::fs::*;
 
 fn main() {
     let tile_set = read_to_string("tile_set.toml").unwrap();
-    let tile_set = TileSet::from_string(&tile_set);
-    println!("Hello, world!");
+    let tile_set = tile_set.parse::<TileSet>().unwrap();
+    println!("{:?}", tile_set);
 }
