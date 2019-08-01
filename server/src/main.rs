706ced4366d05c4fe760ace66a5133f6c4d12354
lbrande <lovbra00@gmail.com>
2019-08-01 18:06:24 +0200

    Moved main.rs to it's new folder.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,8 @@
+use lib::TileSet;
+use std::fs::*;
+
 fn main() {
-    println!("Hello, world!");
+    let tile_set = read_to_string("tile_set.toml").unwrap();
+    let tile_set = tile_set.parse::<TileSet>().unwrap();
+    println!("{:?}", tile_set);
 }
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use lib::TileSet;
-use std::fs::*;
-
-fn main() {
-    let tile_set = read_to_string("tile_set.toml").unwrap();
-    let tile_set = tile_set.parse::<TileSet>().unwrap();
-    println!("{:?}", tile_set);
-}
