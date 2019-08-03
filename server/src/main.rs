cf3c3ea6b8888f1a7509aaaa9061810ca9628eea
lbrande <lovbra00@gmail.com>
2019-08-03 22:08:18 +0200

    Moved TOML to it's own folder and specied contents to count as languages.

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,10 +3,10 @@ use lib::TileSet;
 use std::fs::*;
 
 fn main() {
-    let tile_set = read_to_string("tile_set.toml").unwrap();
+    let tile_set = read_to_string("conf/tile_set.toml").unwrap();
     let tile_set = tile_set.parse::<TileSet>().unwrap();
     println!("{:#?}", tile_set);
-    let map = read_to_string("map.toml").unwrap();
+    let map = read_to_string("conf/map.toml").unwrap();
     let map = map.parse::<Map>().unwrap();
     println!("{:#?}", map);
 }
