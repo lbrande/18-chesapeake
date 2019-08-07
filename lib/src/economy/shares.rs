8e4d6b3e8c49773937684f07275bef6020de8533
lbrande <lovbra00@gmail.com>
2019-08-07 18:30:26 +0200

    Implemented president_shares for Shares and added with_all_shares().

--- lib/src/economy/shares.rs
+++ lib/src/economy/shares.rs
@@ -1,10 +1,11 @@
 use crate::PubComId;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 /// Represents any compination of shares
 #[derive(Clone, Debug, Default)]
 pub struct Shares {
     shares: HashMap<PubComId, u32>,
+    president_shares: HashSet<PubComId>,
 }
 
 impl Shares {
@@ -14,6 +15,23 @@ impl Shares {
         for &id in PubComId::values() {
             shares.insert(id, 0);
         }
-        Self { shares }
+        Self {
+            shares,
+            president_shares: HashSet::new(),
+        }
+    }
+
+    /// Returns an empty `Shares`
+    pub fn with_all_shares() -> Self {
+        let mut shares = HashMap::new();
+        let mut president_shares = HashSet::new();
+        for &id in PubComId::values() {
+            shares.insert(id, 0);
+            president_shares.insert(id);
+        }
+        Self {
+            shares,
+            president_shares,
+        }
     }
 }
